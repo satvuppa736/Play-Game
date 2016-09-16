f94b8a509bbed3b37a3a1cd6a9d2f94318cf3ad6
satvuppa736 <satvuppa736@ed.amdsb.ca>
2016-09-16 13:01:52 -0400

    Add project files.

---
+++ Play Game/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Play_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playGameButton = new System.Windows.Forms.Button();
+            this.startingGameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playGameButton
+            // 
+            this.playGameButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.playGameButton.Font = new System.Drawing.Font("Myriad Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playGameButton.ForeColor = System.Drawing.Color.White;
+            this.playGameButton.Location = new System.Drawing.Point(122, 72);
+            this.playGameButton.Name = "playGameButton";
+            this.playGameButton.Size = new System.Drawing.Size(218, 93);
+            this.playGameButton.TabIndex = 0;
+            this.playGameButton.Text = "Press To Play Game";
+            this.playGameButton.UseVisualStyleBackColor = true;
+            this.playGameButton.Click += new System.EventHandler(this.playGameButton_Click);
+            // 
+            // startingGameLabel
+            // 
+            this.startingGameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.startingGameLabel.Font = new System.Drawing.Font("Myriad Pro", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startingGameLabel.Location = new System.Drawing.Point(122, 178);
+            this.startingGameLabel.Name = "startingGameLabel";
+            this.startingGameLabel.Size = new System.Drawing.Size(218, 51);
+            this.startingGameLabel.TabIndex = 1;
+            this.startingGameLabel.Text = "Starting in 3...";
+            this.startingGameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(475, 391);
+            this.Controls.Add(this.startingGameLabel);
+            this.Controls.Add(this.playGameButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Play Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playGameButton;
+        private System.Windows.Forms.Label startingGameLabel;
+    }
+}
+
---
+++ Play Game/Form1.cs
@@ -0,0 +1,102 @@
+﻿///This program was created by Satvir Uppal 
+///on September 8th 2016
+///Purpose: To complete the formative assignment
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Play_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();          
+
+        }
+
+        private void playGameButton_Click(object sender, EventArgs e)
+        {
+            playGameButton.Visible = false;
+            startingGameLabel.BackColor = Color.Transparent;
+            startingGameLabel.ForeColor = Color.White;
+
+            SoundPlayer player = new SoundPlayer(Properties.Resources.beep1);
+            player.Play();
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            startingGameLabel.Text = "Starting in 2...";
+            new SoundPlayer(Properties.Resources.beep1);
+            player.Play();
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            new SoundPlayer(Properties.Resources.beep1);
+            player.Play();
+            startingGameLabel.Text = "Starting in 1...";
+
+            Refresh();
+            Thread.Sleep(1300);
+
+            player = new SoundPlayer(Properties.Resources.airHorn);
+            player.Play();
+            BackColor = Color.DarkBlue;
+            startingGameLabel.Visible = false;
+
+//Setup for Graphics & Brushes & Pens
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Red, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Blue);
+            Graphics fg = this.CreateGraphics();
+
+//Creates & fills an Ellipse
+            formGraphics.DrawEllipse(drawPen, 140, 100, 200, 150);
+            formGraphics.FillEllipse(drawBrush, 140, 100, 200, 150);
+
+//Draws an "S" larger than other text
+            Font drawFont = new Font("Sitka Text", 70, FontStyle.Bold);
+            SolidBrush drawText = new SolidBrush(Color.Red);
+            fg.DrawString("S", drawFont, drawText, 145, 115);
+
+//Draws a smaller "ATVIR" beside the "S"
+            drawFont = new Font("Dejavu Serif", 30, FontStyle.Regular);
+            drawText = new SolidBrush(Color.Red);
+            fg.DrawString("ATVIR", drawFont, drawText, 203, 150);
+
+//Draws "Uppal Inc." turned 270 degrees & to the right of the Ellipse 
+            drawFont = new Font("Courier Serif", 9, FontStyle.Regular);
+            drawText = new SolidBrush(Color.Black);
+            fg.TranslateTransform(350, 200); 
+            fg.RotateTransform(270); 
+            fg.DrawString("Uppal Inc.", drawFont, drawText, new Rectangle()); 
+            fg.ResetTransform();
+
+            Thread.Sleep(2000);
+
+//Makes the entire screen black for a background for PacMan animation
+            formGraphics.Clear(Color.Black);
+
+            drawBrush = new SolidBrush(Color.Blue);
+            drawPen = new Pen(Color.Blue);
+
+            formGraphics.DrawRectangle(drawPen, 10, 80, 450, 50);
+            formGraphics.FillRectangle(drawBrush, 10, 80, 450, 50);
+            formGraphics.DrawRectangle(drawPen, 10, 210, 270, 50);
+            formGraphics.FillRectangle(drawBrush, 10, 210, 270, 50);
+            formGraphics.DrawRectangle(drawPen, 440, 130, 50, 450);
+            formGraphics.FillRectangle(drawBrush, 440, 130, 50, 450);
+
+        }
+    }
+}
